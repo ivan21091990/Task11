aa26a9dd29167118c6a6d6366bc290308b9e942e
IPAndreev <aip1990andreev@yandex.ru>
2019-08-18 21:20:47 +0300

    Задача 11

---
+++ src/com/itProger/Main.java
@@ -0,0 +1,13 @@
+package com.itProger;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+               System.out.println("Ввод строки: ");
+                String s = scanner.nextLine();
+        System.out.println("Вывод строки: " + s);
+        }
+    }
